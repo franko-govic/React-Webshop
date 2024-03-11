d7a6b50c87e1a8e4219ad0c8e65d311ca515b5f9
franko-govic <fgovic1997@gmail.com>
2024-03-11 19:35:55 +0100

    React Webshop initial setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Main/Main.jsx
@@ -0,0 +1,11 @@
+function MainComponent() {
+
+  return(
+    <>
+    <h2>Main Component</h2>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum labore accusamus vel dicta eaque quaerat beatae delectus sit magni corporis, iste ad repudiandae sunt architecto quasi doloremque facere distinctio ipsum? Quibusdam ipsam perferendis veritatis commodi rerum iure doloremque ullam corporis a debitis aliquam corrupti cumque eveniet, iste, expedita dolores architecto itaque soluta quas dolorem! Quasi consequatur optio quas labore commodi consequuntur sed quam eius impedit aperiam assumenda deleniti aut facere pariatur illo, iure quia ad. Sint sequi cupiditate facere minima doloremque, eius cum enim quia, fugit fuga ullam recusandae magni esse eaque numquam sed atque consequuntur porro officia non repellendus.</p>
+    </>
+    )
+}
+
+export {MainComponent}
